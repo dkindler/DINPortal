12bd210cc3b305cf971567eaec7565dcf9d01e29
Dan Kindler <daniel.kindler1@gmail.com>
2017-07-10 22:41:28 -0400

    add new din creation

--- dinport.py
+++ dinport.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/din/<din>")
-def profile(din):
+def view(din):
     return render_template("view.html", din=din)
+
+@app.route("/new")
+def create():
+    return render_template("new.html")
